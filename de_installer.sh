4de385344ecfa5a3ae2d337f70e34eb13986ed68
Salih Emin <cerebrux@users.noreply.github.com>
2020-05-16 16:02:41 +0300

    Update de_installer.sh

--- de_installer.sh
+++ de_installer.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#
+#
+# Archon -- Ελληνικός Arch Linux Installer
+# Copyright (c)2017 Vasilis Niakas, Salih Emin and Contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation version 3 of the License.
+#
+# Please read the file LICENSE, README and AUTHORS for more information.
+#
+#
+
+
 # Exit codes για success || failure
 #
 
